c17045ba4bf18b3507e2718b5c08c8597cbe331b
Boyang Gao <boyang.gao@yahoo.com>
2023-04-13 23:59:22 -0700

    add sensetive analysis

--- car_predict_model.py
+++ car_predict_model.py
@@ -1,4 +1,8 @@
 from abc import abstractmethod
+import torch
+import joblib
+
+from ann.model.ANN import ANN
 
 
 class CarPredictionModel:
@@ -10,3 +14,20 @@ class CarPredictionModel:
     @abstractmethod
     def predict(self, X) -> float:
         pass
+
+
+class ANNPredictionModel(CarPredictionModel):
+    def __init__(self, model_path):
+        self.model = ANN(120, hidden_size=1024, output_size=1)
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.eval()
+
+    def predict(self, X) -> float:
+        preprocessor = joblib.load("ann/preprocessor.joblib")
+        price_scaler = joblib.load("ann/price_scaler.joblib")
+        X = preprocessor.transform(X)
+        X = torch.tensor(X, dtype=torch.float)  # Convert DataFrame to NumPy array and then to tensor
+        y = self.model(X)
+        y = y.detach().numpy()
+        y = price_scaler.inverse_transform(y)
+        return y[0][0]
--- main.py
+++ main.py
@@ -3,25 +3,8 @@ import numpy as np
 import pandas as pd
 import torch
 
-from car_predict_model import CarPredictionModel
+from car_predict_model import ANNPredictionModel
 from ann.model.ANN import ANN
-
-
-class ANNPredictionModel(CarPredictionModel):
-    def __init__(self, model_path):
-        self.model = ANN(120, hidden_size=1024, output_size=1)
-        self.model.load_state_dict(torch.load(model_path))
-        self.model.eval()
-
-    def predict(self, X) -> float:
-        preprocessor = joblib.load("ann/preprocessor.joblib")
-        price_scaler = joblib.load("ann/price_scaler.joblib")
-        X = preprocessor.transform(X)
-        X = torch.tensor(X, dtype=torch.float)  # Convert DataFrame to NumPy array and then to tensor
-        y = self.model(X)
-        y = y.detach().numpy()
-        y = price_scaler.inverse_transform(y)
-        return y[0][0]
 
 
 def print_options(options):

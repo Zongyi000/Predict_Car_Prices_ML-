8c1044f90c495aeeb7749df51bb1dcdde442ec02
Zongyi Jiang <59304487+Zongyi000@users.noreply.github.com>
2023-04-14 12:02:07 -0700

    Update DTR_fast_run_using_model.py

--- decision_tree_regressor/DTR_fast_run_using_model.py
+++ decision_tree_regressor/DTR_fast_run_using_model.py
@@ -43,11 +43,72 @@ def main():
     print(f"RMSE: {sqrt(mse):.2f}")
     print(f"R^2 score: {r2:.2f}")
 
+    # Compare predicted and actual prices
+    print("Predicted Prices vs. Actual Prices")
+    for pred, actual in zip(y_pred[:10], y_test[:10]):
+        print(f"Predicted: {pred:.2f} | Actual: {actual:.2f}")
+
     # Evaluate feature importance
     feature_importance = loaded_regressor.feature_importances_
+    print("feature importance below:")
     for feature, importance in zip(X.columns, feature_importance):
         print(f"{feature}: {importance:.4f}")
+    print("-----")
+
+    # Sensitivity analysis
+    sensitivity_analysis = {}
+    for feature in X.columns:
+        X_test_copy = X_test.copy()
+        X_test_copy[feature] *= 1.05
+        y_pred_copy = loaded_regressor.predict(X_test_copy)
+        mse_copy = mean_squared_error(y_test, y_pred_copy)
+        sensitivity_analysis[feature] = (mse_copy - mse) / mse
+
+    # Print sensitivity analysis results
+    print("\nSensitivity Analysis:")
+    for feature, sensitivity in sensitivity_analysis.items():
+        print(f"{feature}: {sensitivity:.4f}")
+
+    # Inverse transform the standardized values
+    X_test[numerical_features] = scaler.inverse_transform(X_test[numerical_features])
 
 
 if __name__ == "__main__":
     main()
+
+
+
+# Mean squared error: 31985992.80
+# RMSE: 5655.62
+# R^2 score: 0.80
+# Predicted Prices vs. Actual Prices
+# Predicted: 6274.66 | Actual: 7988.00
+# Predicted: 10465.26 | Actual: 18500.00
+# Predicted: 21086.67 | Actual: 15900.00
+# Predicted: 6744.70 | Actual: 8200.00
+# Predicted: 7350.24 | Actual: 5900.00
+# Predicted: 23065.00 | Actual: 22590.00
+# Predicted: 15371.93 | Actual: 18990.00
+# Predicted: 5905.00 | Actual: 4500.00
+# Predicted: 34352.54 | Actual: 20500.00
+# Predicted: 20523.33 | Actual: 17990.00
+# feature importance below:
+# year: 0.5639
+# manufacturer: 0.0884
+# fuel: 0.0792
+# odometer: 0.1103
+# title_status: 0.0067
+# transmission: 0.0172
+# type: 0.1236
+# state: 0.0108
+# -----
+#
+# Sensitivity Analysis:
+# year: 0.0015
+# manufacturer: 0.5070
+# fuel: 0.0000
+# odometer: 0.1913
+# title_status: 0.0000
+# transmission: -0.0003
+# type: 0.0247
+# state: 0.0067
